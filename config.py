5e69ee286a94baa6854fc54a4e9307b40c4769d6
Arpitgavla <164368538+Arpitgavla@users.noreply.github.com>
2024-08-06 23:15:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ API_HASH = environ.get("API_HASH", "07cd9c82699c28111cb33693ecbd9116")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6133440326').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "File_Press_bot") # without @
 PORT = environ.get("PORT", "8080")
 
